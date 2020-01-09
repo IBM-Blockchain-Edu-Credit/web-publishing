2c74d95529a4db3b5a1f43106ca64667a6dfda56
KIMdolphin <rlacofls0212@naver.com>
2020-01-09 20:48:40 +0900

    1/9 작업분
    현재 HTTP 500 오류 발생

--- html/index.php
+++ html/index.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>OPP TEST</title>
-  </head>
-  <body>
-    우왕 된당
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>OPP TEST</title>
+
+  <script src="lib/jquery/jquery-3.3.1.min.js"></script>
+  <script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
+  <link href="lib/bootstrap/dist/css/bootstrap.css" rel="stylesheet" />
+  <link href="css/fonts.css" rel="stylesheet" />
+</head>
+<?php
+  require $_SERVER["DOCUMENT_ROOT"].'/scripts/dbconnect.php';
+  $connect = DBConnect();
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+?>
+<body>
+ <center><br>
+   <h1>hi, OPP!</h1>
+   <?php
+     $result = mysqli_query($connect, SELECT * FROM 'question' WHERE questnum = 1);
+     echo "<h3>" . $row[1] . "</h3>";
+     echo "<h3>" . $row[2] . "</h3>";
+     echo "<h3>" . $row[3] . "</h3>";
+     echo "<h3>" . $row[4] . "</h3>";
+     echo "<h3>" . $row[5] . "</h3>";
+   ?>
+</body>
 </html>
---
+++ html/scripts/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+function DBConnect()
+{
+    $connect = new mysqli('호스트명','아이디', '비번', 'db', '포트');
+    if($connect->connect_errno){
+        echo '[연결실패] : '.$connect->connect_error;
+        exit;
+    }
+    if(!$connect->set_charset('utf8'))// (php >= 5.0.5)
+    {
+        echo '[문자열변경실패] : '.$connect->connect_error;
+        exit;
+    }
+    $connect->query('set character set euckr;');
+    $connect->query("set session character_set_connection=utf8;");
+    $connect->query("set session character_set_results=utf8;");
+    $connect->query("set session character_set_client=utf8;"); //여기까지 공통
+    return $connect;
+}
+?>
